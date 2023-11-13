4fb6047601ffa11aeb658f8bc349cdd670626782
nikey368 <marcusing2009@gmail.com>
2023-11-13 11:41:36 +1100

    sorry i haven't worked on this in a while

--- js/bakaomoji.js
+++ js/bakaomoji.js
@@ -1,18 +1,18 @@
 const dropdown = document.getElementById("感情")
 const emotions = {
-    "中性(neutral)": {leftEye:"・",rightEye:"・",mouth:"ー"},
-    "ハッピー(happy)": {leftEye:"・",rightEye:"・",mouth:"⋃"},
-    "悲しい(sad)": {leftEye:"・",rightEye:"・",mouth:"⋂"},
-    "驚いた(surprised)": {leftEye:"・",rightEye:"・",mouth:"Ｏ"},
-    "とても驚いた(very surprised)": {leftEye:"⦿",rightEye:"⦿",mouth:"Ｏ"},
+    "中性": {leftEye:"・",rightEye:"・",mouth:"ー"},
+    "ハッピー": {leftEye:"・",rightEye:"・",mouth:"⋃"},
+    "悲しい": {leftEye:"・",rightEye:"・",mouth:"⋂"},
+    "驚いた": {leftEye:"・",rightEye:"・",mouth:"Ｏ"},
+    "とても驚いた": {leftEye:"⦿",rightEye:"⦿",mouth:"Ｏ"},
 }
 var leftEye = "・"
 var rightEye = "・"
 var mouth = "ー"
 let untilnextminute = setInterval(function() {
-    leftEye = emotions[dropdown.options[dropdown.selectedIndex].text]["leftEye"]
-    rightEye = emotions[dropdown.options[dropdown.selectedIndex].text]["rightEye"]
-    mouth = emotions[dropdown.options[dropdown.selectedIndex].text]["mouth"]
+    leftEye = emotions[dropdown.options[dropdown.selectedIndex].value]["leftEye"]
+    rightEye = emotions[dropdown.options[dropdown.selectedIndex].value]["rightEye"]
+    mouth = emotions[dropdown.options[dropdown.selectedIndex].value]["mouth"]
     let randomEye = Math.random()
     let setlefteye = randomEye < 0.1 ? "-" : leftEye
     let setrighteye = randomEye < 0.1 ? "-" : rightEye
@@ -21,9 +21,7 @@ let untilnextminute = setInterval(function() {
 }, 100 );
 
 function copyFace() {
-   // Copy the text inside the text field
   navigator.clipboard.writeText(leftEye+mouth+rightEye);
 
-  // Alert the copied text
   alert("コピーに成功しました：" + leftEye+mouth+rightEye);
 }
